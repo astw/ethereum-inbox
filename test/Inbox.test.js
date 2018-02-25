6233ed7740a4d0b131ef9c96a8b5fd42f48c3daa
shuhaowang <wshuhao@gmail.com>
2018-02-25 15:52:37 -0800

    verify the test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,15 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
-const {interface, bytecode} = require('../compile'); 
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const {interface, bytecode} = require('../compile');
 
 let accounts;
 let inbox;
+let inital_string = "Hi there";
 
 beforeEach(async ()=>{
   // Get a list of all accounts;
@@ -21,5 +25,25 @@ beforeEach(async ()=>{
 describe('Inbox', ()=>{
   it('deploys a contract', ()=>{
      console.log(inbox);
+     // if the contrac is deploy successfully, it should has an address
+     assert.ok(inbox.options.address);
   });
+
+  it('has a defautl message', async ()=>{
+      const message = await inbox.methods.message().call();
+      assert.equal(message, inital_string);
+
+  });
+
+ it('can change the message', async ()=>{
+   // send a transaction
+   await inbox.methods.setMessage("bye").send({from: accounts[0]});
+
+   const message = await inbox.methods.message().call();
+
+   assert.equal(message, 'bye');
+ });
+
+
+
 });
