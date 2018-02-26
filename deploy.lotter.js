5215b4eaaafe271940599f5612d9f0189b94cc7f
shuhaowang <wshuhao@gmail.com>
2018-02-25 20:05:40 -0800

    add lottery smart contract:

---
+++ deploy.lotter.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require("web3");
+const {interface, bytecode} = require('./compile');
+
+var mnemoic = 'weasel various about woman label guard initial dove famous barely planet indicate';
+
+const provider = new HDWalletProvider(
+   mnemoic,
+   'https://rinkeby.infura.io/CAV5MLnXGBDc1fZslbyP'
+);
+
+const web3 =  new Web3(provider);
+
+const deploy = async () => {
+   const accounts = await web3.eth.getAccounts();
+
+   console.log(accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode})
+    .send({gas:'1000000', from: accounts[0]});
+};
+
+
+
+deploy();
